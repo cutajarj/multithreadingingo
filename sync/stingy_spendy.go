8e48ac5638a972ffd4b003b88379949e691edbe8
cutajarj <james.cutajar@gmail.com>
2019-12-22 19:45:50 +0000

    Adding stingy and spendy

---
+++ sync/stingy_spendy.go
@@ -0,0 +1,28 @@
+package main
+
+import "time"
+
+var money = 100
+
+func stingy() {
+	for i := 1; i <= 1000; i++ {
+		money += 10
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("Stingy Done")
+}
+
+func spendy() {
+	for i := 1; i <= 1000; i++ {
+		money -= 10
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("Spendy Done")
+}
+
+func main() {
+	go stingy()
+	go spendy()
+	time.Sleep(3000 * time.Millisecond)
+	print(money)
+}
