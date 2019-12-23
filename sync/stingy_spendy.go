3d3d80c062b749ccbad8a0fc42a67feac8640f3a
cutajarj <james.cutajar@gmail.com>
2019-12-23 18:14:26 +0000

    Adding lcoking to stingy and spendy

--- sync/stingy_spendy.go
+++ sync/stingy_spendy.go
@@ -1,12 +1,20 @@
 package main
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
-var money = 100
+var (
+	money = 100
+	lock  = sync.Mutex{}
+)
 
 func stingy() {
 	for i := 1; i <= 1000; i++ {
+		lock.Lock()
 		money += 10
+		lock.Unlock()
 		time.Sleep(1 * time.Millisecond)
 	}
 	println("Stingy Done")
@@ -14,7 +22,9 @@ func stingy() {
 
 func spendy() {
 	for i := 1; i <= 1000; i++ {
+		lock.Lock()
 		money -= 10
+		lock.Unlock()
 		time.Sleep(1 * time.Millisecond)
 	}
 	println("Spendy Done")
