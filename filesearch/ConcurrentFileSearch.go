6930ef22c770c2065dc7c50e830b336a18e7fb84
cutajarj <james.cutajar@gmail.com>
2019-12-29 10:42:07 +0000

    Adding multi threaded file search

---
+++ filesearch/ConcurrentFileSearch.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+var (
+	matches   []string
+	waitgroup = sync.WaitGroup{}
+	lock      = sync.Mutex{}
+)
+
+func fileSearch(root string, filename string) {
+	fmt.Println("Searching in " + root)
+	files, _ := ioutil.ReadDir(root)
+	for _, file := range files {
+		if strings.Contains(file.Name(), filename) {
+			lock.Lock()
+			matches = append(matches, filepath.Join(root, file.Name()))
+			lock.Unlock()
+		}
+		if file.IsDir() {
+			waitgroup.Add(1)
+			go fileSearch(filepath.Join(root, file.Name()), filename)
+		}
+	}
+	waitgroup.Done()
+}
+
+func main() {
+	waitgroup.Add(1)
+	go fileSearch("C:/tools", "awk.sh")
+	waitgroup.Wait()
+	fmt.Println("*********** RESULTS ***********")
+	for _, file := range matches {
+		fmt.Println(file)
+	}
+}
