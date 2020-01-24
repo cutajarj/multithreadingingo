3685ba09ee960f2479a398a066a2ffe531959256
cutajarj <james.cutajar@gmail.com>
2020-01-24 16:52:59 +0000

    Using more regex for wind direction implementation

--- channels/WindDIrection.go
+++ channels/WindDIrection.go
@@ -12,8 +12,14 @@ import (
 )
 
 var (
-	windRegex = regexp.MustCompile(`\d* METAR.*EGLL \d*Z [A-Z ]*(\d\d\d|VRB).*=`)
-	windDist  [8]int
+	windRegex     = regexp.MustCompile(`\d* METAR.*EGLL \d*Z [A-Z ]*(\d{5}KT|VRB\d{2}KT).*=`)
+	tafValidation = regexp.MustCompile(`.*TAF.*`)
+	comment       = regexp.MustCompile(`\w*#.*`)
+	metarClose    = regexp.MustCompile(`.*=`)
+	variableWind  = regexp.MustCompile(`.*VRB\d{2}KT`)
+	validWind     = regexp.MustCompile(`\d{5}KT`)
+	windDirOnly   = regexp.MustCompile(`(\d{3})\d{2}KT`)
+	windDist      [8]int
 )
 
 func convertToArray(text string) []string {
@@ -21,13 +27,13 @@ func convertToArray(text string) []string {
 	metarSlice := make([]string, 0, len(lines))
 	metarStr := ""
 	for _, line := range lines {
-		if strings.Contains(line, "TAF") {
+		if tafValidation.MatchString(line) {
 			break
 		}
-		if !strings.HasPrefix(line, "#") {
+		if !comment.MatchString(line) {
 			metarStr += strings.Trim(line, " ")
 		}
-		if strings.HasSuffix(line, "=") {
+		if metarClose.MatchString(line) {
 			metarSlice = append(metarSlice, metarStr)
 			metarStr = ""
 		}
@@ -47,9 +53,16 @@ func extractWindDirection(metars []string) []string {
 
 func extractWindDistribution(winds []string) {
 	for _, wind := range winds {
-		if d, err := strconv.ParseFloat(wind, 64); err == nil {
-			dirIndex := int(math.Round(d/45.0)) % 8
-			windDist[dirIndex]++
+		if variableWind.MatchString(wind) {
+			for i := 0; i < 8; i++ {
+				windDist[i]++
+			}
+		} else if validWind.MatchString(wind) {
+			windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
+			if d, err := strconv.ParseFloat(windStr, 64); err == nil {
+				dirIndex := int(math.Round(d/45.0)) % 8
+				windDist[dirIndex]++
+			}
 		}
 	}
 }
@@ -60,19 +73,20 @@ func main() {
 	start := time.Now()
 	for _, file := range files {
 		dat, err := ioutil.ReadFile(filepath.Join(absPath, file.Name()))
-		//dat, err := ioutil.ReadFile(filepath.Join(absPath, "apr2013.txt"))
 		if err != nil {
 			panic(err)
 		}
 		text := string(dat)
 		//1. Change to array, each metar report is a separate item in the array
 		metars := convertToArray(text)
-		//2. Regex extract wind direction, EGLL 312350Z 07004KT CAVOK 12/09 Q1016 NOSIG= -> 070
+
+		//2. Extract wind direction, EGLL 312350Z 07004KT CAVOK 12/09 Q1016 NOSIG= -> 070
 		winds := extractWindDirection(metars)
+
 		//3. Assign to N, NE, E, SE, S, SW, W, NW, 070 -> E + 1
 		extractWindDistribution(winds)
 	}
 	elapsed := time.Since(start)
 	fmt.Printf("%v\n", windDist)
-	fmt.Printf("Processing took %s", elapsed)
+	fmt.Printf("Processing   took %s\n", elapsed)
 }
