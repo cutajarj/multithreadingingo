f643a1f2778ca423148c1a57e5b8d6071bfaed3c
cutajarj <james.cutajar@gmail.com>
2020-01-28 12:12:55 +0000

    Wind direction impl with channels

--- channels/WindDIrection.go
+++ channels/WindDIrection.go
@@ -22,52 +22,74 @@ var (
 	windDist      [8]int
 )
 
-func parseToArray(text string) []string {
-	lines := strings.Split(text, "\n")
-	metarSlice := make([]string, 0, len(lines))
-	metarStr := ""
-	for _, line := range lines {
-		if tafValidation.MatchString(line) {
-			break
-		}
-		if !comment.MatchString(line) {
-			metarStr += strings.Trim(line, " ")
-		}
-		if metarClose.MatchString(line) {
-			metarSlice = append(metarSlice, metarStr)
-			metarStr = ""
-		}
-	}
-	return metarSlice
-}
-
-func extractWindDirection(metars []string) []string {
-	winds := make([]string, 0, len(metars))
-	for _, metar := range metars {
-		if windRegex.MatchString(metar) {
-			winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
-		}
-	}
-	return winds
-}
-
-func mineWindDistribution(winds []string) {
-	for _, wind := range winds {
-		if variableWind.MatchString(wind) {
-			for i := 0; i < 8; i++ {
-				windDist[i]++
+func parseToArray(textChannel chan string, metarChannel chan []string) {
+	for text := range textChannel {
+		lines := strings.Split(text, "\n")
+		metarSlice := make([]string, 0, len(lines))
+		metarStr := ""
+		for _, line := range lines {
+			if tafValidation.MatchString(line) {
+				break
 			}
-		} else if validWind.MatchString(wind) {
-			windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
-			if d, err := strconv.ParseFloat(windStr, 64); err == nil {
-				dirIndex := int(math.Round(d/45.0)) % 8
-				windDist[dirIndex]++
+			if !comment.MatchString(line) {
+				metarStr += strings.Trim(line, " ")
+			}
+			if metarClose.MatchString(line) {
+				metarSlice = append(metarSlice, metarStr)
+				metarStr = ""
 			}
 		}
+		metarChannel <- metarSlice
 	}
+	close(metarChannel)
+}
+
+func extractWindDirection(metarChannel chan []string, windsChannel chan []string) {
+	for metars := range metarChannel {
+		winds := make([]string, 0, len(metars))
+		for _, metar := range metars {
+			if windRegex.MatchString(metar) {
+				winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
+			}
+		}
+		windsChannel <- winds
+	}
+	close(windsChannel)
+}
+
+func mineWindDistribution(windsChannel chan []string, distChannel chan [8]int) {
+	for winds := range windsChannel {
+		for _, wind := range winds {
+			if variableWind.MatchString(wind) {
+				for i := 0; i < 8; i++ {
+					windDist[i]++
+				}
+			} else if validWind.MatchString(wind) {
+				windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
+				if d, err := strconv.ParseFloat(windStr, 64); err == nil {
+					dirIndex := int(math.Round(d/45.0)) % 8
+					windDist[dirIndex]++
+				}
+			}
+		}
+	}
+	distChannel <- windDist
+	close(distChannel)
 }
 
 func main() {
+	textChannel := make(chan string)
+	metarChannel := make(chan []string)
+	windsChannel := make(chan []string)
+	resultsChannel := make(chan [8]int)
+
+	//1. Change to array, each metar report is a separate item in the array
+	go parseToArray(textChannel, metarChannel)
+	//2. Extract wind direction, EGLL 312350Z 07004KT CAVOK 12/09 Q1016 NOSIG= -> 070
+	go extractWindDirection(metarChannel, windsChannel)
+	//3. Assign to N, NE, E, SE, S, SW, W, NW, 070 -> E + 1
+	go mineWindDistribution(windsChannel, resultsChannel)
+
 	absPath, _ := filepath.Abs("./multithreadingingo/metarfiles/")
 	files, _ := ioutil.ReadDir(absPath)
 	start := time.Now()
@@ -77,16 +99,11 @@ func main() {
 			panic(err)
 		}
 		text := string(dat)
-		//1. Change to array, each metar report is a separate item in the array
-		metarsReports := parseToArray(text)
-
-		//2. Extract wind direction, EGLL 312350Z 07004KT CAVOK 12/09 Q1016 NOSIG= -> 070
-		windsDirections := extractWindDirection(metarsReports)
-
-		//3. Assign to N, NE, E, SE, S, SW, W, NW, 070 -> E + 1
-		mineWindDistribution(windsDirections)
+		textChannel <- text
 	}
+	close(textChannel)
+	results := <-resultsChannel
 	elapsed := time.Since(start)
-	fmt.Printf("%v\n", windDist)
-	fmt.Printf("Processing   took %s\n", elapsed)
+	fmt.Printf("%v\n", results)
+	fmt.Printf("Processing took %s\n", elapsed)
 }
