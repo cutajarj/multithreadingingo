1ca3659d5d2803535b26de3f29f7c87823256054
cutajarj <james.cutajar@gmail.com>
2020-01-29 09:45:02 +0000

    modified stingy_spendy for cond variables

---
+++ condvariables/stingy_spendy_waits.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	money          = 10
+	lock           = sync.Mutex{}
+	moneyDeposited = sync.NewCond(&lock)
+)
+
+func stingy() {
+	for i := 1; i <= 1000; i++ {
+		lock.Lock()
+		money += 10
+		fmt.Println("Stingy sees balance of ", money)
+		moneyDeposited.Signal()
+		lock.Unlock()
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("Stingy Done")
+}
+
+func spendy() {
+	for i := 1; i <= 1000; i++ {
+		lock.Lock()
+		for money-20 < 0 {
+			moneyDeposited.Wait()
+		}
+		money -= 20
+		fmt.Println("Spendy sees balance of ", money)
+		lock.Unlock()
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("Spendy Done")
+}
+
+func main() {
+	go stingy()
+	go spendy()
+	time.Sleep(3000 * time.Millisecond)
+	print(money)
+}
