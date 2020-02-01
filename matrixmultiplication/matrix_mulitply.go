39c5f648d04859826566a65643dfafde58896b1c
cutajarj <james.cutajar@gmail.com>
2020-02-01 11:24:40 +0000

    Adding multithreaded matrix multiplication

--- matrixmultiplication/matrix_mulitply.go
+++ matrixmultiplication/matrix_mulitply.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -11,39 +12,53 @@ const (
 )
 
 var (
-	matrixA = [matrixSize][matrixSize]int{}
-	matrixB = [matrixSize][matrixSize]int{}
-	result  = [matrixSize][matrixSize]int{}
+	matrixA   = [matrixSize][matrixSize]int{}
+	matrixB   = [matrixSize][matrixSize]int{}
+	result    = [matrixSize][matrixSize]int{}
+	rwLock    = sync.RWMutex{}
+	cond      = sync.NewCond(rwLock.RLocker())
+	waitGroup = sync.WaitGroup{}
 )
 
 func generateRandomMatrix(matrix *[matrixSize][matrixSize]int) {
 	for row := 0; row < matrixSize; row++ {
 		for col := 0; col < matrixSize; col++ {
-			matrix[row][col] = rand.Intn(10) - 5
+			matrix[row][col] += rand.Intn(10) - 5
 		}
 	}
 }
 
 func workOutRow(row int) {
-	for col := 0; col < matrixSize; col++ {
-		for i := 0; i < matrixSize; i++ {
-			result[row][col] += matrixA[row][i] * matrixB[i][col]
+	rwLock.RLock()
+	for {
+		waitGroup.Done()
+		cond.Wait()
+		for col := 0; col < matrixSize; col++ {
+			for i := 0; i < matrixSize; i++ {
+				result[row][col] += matrixA[row][i] * matrixB[i][col]
+			}
 		}
 	}
 }
 
 func main() {
 	fmt.Println("Working...")
+	waitGroup.Add(matrixSize)
+	for row := 0; row < matrixSize; row++ {
+		go workOutRow(row)
+	}
+
 	start := time.Now()
 	for i := 0; i < 100; i++ {
+		waitGroup.Wait()
+		rwLock.Lock()
 		generateRandomMatrix(&matrixA)
-		generateRandomMatrix(&matrixA)
-
-		for row := 0; row < matrixSize; row++ {
-			workOutRow(row)
-		}
+		generateRandomMatrix(&matrixB)
+		waitGroup.Add(matrixSize)
+		rwLock.Unlock()
+		cond.Broadcast()
 	}
 	elapsed := time.Since(start)
 	fmt.Println("Done")
-	fmt.Printf("Processing took %s\n", elapsed)
+	fmt.Println("Processing took %s\n", elapsed)
 }
