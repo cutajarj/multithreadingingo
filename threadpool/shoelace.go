4fc69cefba80d41b5e68860cf401fbeacec59c04
cutajarj <james.cutajar@gmail.com>
2020-02-05 13:03:22 +0000

    Making shoelace multi threaded

--- threadpool/shoelace.go
+++ threadpool/shoelace.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -16,40 +17,48 @@ type Point2D struct {
 	y int
 }
 
+const numberOfThreads int = 8
+
 var (
-	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	r         = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	waitGroup = sync.WaitGroup{}
 )
 
-func findArea(pointsStr string) float64 {
-	var points []Point2D
-	for _, p := range r.FindAllStringSubmatch(pointsStr, -1) {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2D{x, y})
-	}
+func findArea(inputChannel chan string) {
+	for pointsStr := range inputChannel {
+		var points []Point2D
+		for _, p := range r.FindAllStringSubmatch(pointsStr, -1) {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{x, y})
+		}
 
-	area := 0.0
-	for i := 0; i < len(points); i++ {
-		a, b := points[i], points[(i+1)%len(points)]
-		area += float64(a.x*b.y) - float64(a.y*b.x)
+		area := 0.0
+		for i := 0; i < len(points); i++ {
+			a, b := points[i], points[(i+1)%len(points)]
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+		fmt.Println(math.Abs(area) / 2.0)
 	}
-	return math.Abs(area) / 2.0
+	waitGroup.Done()
 }
 
 func main() {
 	absPath, _ := filepath.Abs("./multithreadingingo/threadpool/")
-	dat, err := ioutil.ReadFile(filepath.Join(absPath, "polygons.txt"))
-	if err != nil {
-		panic(err)
-	}
+	dat, _ := ioutil.ReadFile(filepath.Join(absPath, "polygons.txt"))
 	text := string(dat)
 
+	inputChannel := make(chan string, 1000)
+	for i := 0; i < numberOfThreads; i++ {
+		go findArea(inputChannel)
+	}
+	waitGroup.Add(numberOfThreads)
 	start := time.Now()
 	for _, line := range strings.Split(text, "\n") {
-		//(3,4),(5,11),(12,8),(9,5),(5,6)
-		findArea(line)
-		//fmt.Println("Area is ", ans)
+		inputChannel <- line
 	}
+	close(inputChannel)
+	waitGroup.Wait()
 	elapsed := time.Since(start)
-	fmt.Printf("Processing took %s\n", elapsed)
+	fmt.Printf("Processing took %s \n", elapsed)
 }
