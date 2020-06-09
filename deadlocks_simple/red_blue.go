8cfade5f6a3299f17297c22696f7ebb8a6140080
cutajarj <james.cutajar@gmail.com>
2020-06-09 09:29:32 +0100

    improving logging for red_blue deadlock

--- deadlocks_simple/red_blue.go
+++ deadlocks_simple/red_blue.go
@@ -13,25 +13,27 @@ var (
 
 func blueRobot() {
 	for {
-		fmt.Println("Blue: Acquiring first lock...")
+		fmt.Println("Blue: Acquiring lock1...")
 		lock1.Lock()
-		fmt.Println("Blue: Acquiring second lock...")
+		fmt.Println("Blue: Acquiring lock2...")
 		lock2.Lock()
 		fmt.Println("Blue: Locks Acquired")
 		lock2.Unlock()
 		lock1.Unlock()
+		fmt.Println("Blue: Locks Released")
 	}
 }
 
 func redRobot() {
 	for {
-		fmt.Println("Red: Acquiring first lock...")
+		fmt.Println("Red: Acquiring lock2...")
 		lock2.Lock()
-		fmt.Println("Red: Acquiring second lock...")
+		fmt.Println("Red: Acquiring lock1...")
 		lock1.Lock()
 		fmt.Println("Red: Locks Acquired")
 		lock1.Unlock()
 		lock2.Unlock()
+		fmt.Println("Red: Locks Released")
 	}
 }
 
