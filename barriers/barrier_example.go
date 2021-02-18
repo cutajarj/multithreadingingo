42107bebff755ea8a3ef3214b7209b1673776f65
cutajarj <james.cutajar@gmail.com>
2021-02-18 20:01:50 +0000

    Adding Barriers

--- barriers/barrier_example.go
+++ barriers/barrier_example.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "time"
 
+func waitOnBarrier(name string, timeToSleep int, barrier *Barrier) {
+	for {
+		println(name, "running")
+		time.Sleep(time.Duration(timeToSleep) * time.Second)
+		println(name, "is waiting on barrier")
+		barrier.Wait()
+	}
+}
+
+func main() {
+	barrier := NewBarrier(2)
+	go waitOnBarrier("red", 4, barrier)
+	go waitOnBarrier("blue", 10, barrier)
+	time.Sleep(time.Duration(100) * time.Second)
 }
